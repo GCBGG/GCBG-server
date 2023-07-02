3f45a5a5af5426199d3130ed0ed7d4ed2258e41e
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2023-07-02 21:35:39 +0900

    FEAT: 로그인 API 페이지 분리

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const loginRouter = require('./routes/login');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/api/login', loginRouter);
 
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+// CORS 하용 설정하기.
+app.use((req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 
 app.listen(port, () => {
